f8ae4437eca2a489c7dffdbc7ce1eefce3963592
Sydney Bridges <sybrid@gmail.com>
2017-10-24 14:15:41 -0400

    Unit Test for model checking fields are not empty

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-    with:     %r{\.(gif|jpg|png)\Z}i,
-    message:  'must be a url for a gif, jpg, or png image'
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a url for a gif, jpg, or png image'
   }
 end
