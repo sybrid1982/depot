e8692aaeb0538d85d3fb175303844743fab7a665
Sydney Bridges <sybrid@gmail.com>
2017-10-25 10:48:37 -0400

    Added raise exception to number_to_currency in store/index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_date_loaded
   protect_from_forgery with: :exception
+
+
+
+  private
+  def set_date_loaded
+    @date_loaded = Time.now
+  end
 end
