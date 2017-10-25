d21b687d47fd9589b708d756a38f97449b248d54
Sydney Bridges <sybrid@gmail.com>
2017-10-25 11:28:52 -0400

    Test that can't destroy product in cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -7,4 +10,12 @@ class Product < ApplicationRecord
     message: 'must be a url for a gif, jpg, or png image'
   }
   validates :title, length: {minimum: 10, message: 'Title too short'}
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
