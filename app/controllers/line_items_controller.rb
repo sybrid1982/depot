0a33784b7d87cd0590dcec13ec14d30ff42ce1c8
Sydney Bridges <sybrid@gmail.com>
2017-10-27 13:50:39 -0400

    Ajax now working for decrement button.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -72,10 +72,16 @@ class LineItemsController < ApplicationController
     @line_item = @cart.line_items.find_by_id(params[:id])
     @line_item.decrement
 
-    @line_item.save!
-
     respond_to do |format|
-      format.html {redirect_to store_index_url}
+      if @line_item.save
+        format.html { redirect_to store_index_url }
+        format.js {@current_item = @line_item}
+        format.json { render :show, status: :created, location: @line_item }
+      else
+        format.html { render :new }
+        format.js {@current_item = @line_item}
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
     end
   end
 
