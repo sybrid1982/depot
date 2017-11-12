863909e6fea9f1296f21b21b32a61ab966cd5aab
Sydney Bridges <sybrid@gmail.com>
2017-11-12 16:57:01 -0500

    Login required now before accessing most pages, exceptions are orders, sessions, store

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_date_loaded
+  before_action :authorize
   protect_from_forgery with: :exception
 
 
@@ -7,5 +8,12 @@ class ApplicationController < ActionController::Base
   private
   def set_date_loaded
     @date_loaded = Time.now
+  end
+
+  protected
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice: "Please log in"
+    end
   end
 end
