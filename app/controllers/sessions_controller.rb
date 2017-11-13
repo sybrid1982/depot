2d867bd0f65d63385b178ed1d4ac5fdfd28ac4fb
Sydney Bridges <sybrid@gmail.com>
2017-11-13 16:14:18 -0500

    Should redirect from login to admin page with any information if there are no admins (allows for rapid creation of actual admin)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:name])
-    if user.try(:authenticate, params[:password])
-      session[:user_id] = user.id
-      redirect_to admin_url
+    unless User.count == 0
+      user = User.find_by(name: params[:name])
+      if user.try(:authenticate, params[:password])
+        session[:user_id] = user.id
+        redirect_to admin_url
+      else
+        redirect_to login_url, alert: "Invalid user/password combination"
+      end
     else
-      redirect_to login_url, alert: "Invalid user/password combination"
+      redirect_to admin_url
     end
   end
 
