eda005663acb1ae2376c899dc0d5f6f7a1c6e457
Sydney Bridges <sybrid@gmail.com>
2017-11-13 16:20:13 -0500

    Added test for users_controller to ensure that last user cannot be deleted

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
+    @user2 = users(:two)
   end
 
   test "should get index" do
@@ -44,5 +45,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to users_url
+  end
+
+  test "should not destroy last user" do
+    assert_difference('User.count', -1) do
+      delete user_url(@user)
+    end
+    assert_no_difference('User.count') do
+      delete user_url(@user2)
+    end
   end
 end
